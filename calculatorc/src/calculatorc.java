34c0251675a19a0cb00fc82b445ce74f2eed4017
byhewdota22 <76906945+byhewdota22@users.noreply.github.com>
2024-02-27 21:00:48 +0500

    demo

---
+++ calculatorc/src/calculatorc.java
@@ -0,0 +1,11 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class calculatorc {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+
+        for (int i = 1; i <= 8; i++) {
+            System.out.println("i = " + i);// увше
+        }
+    }
+}
